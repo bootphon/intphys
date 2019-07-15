7325f464ea20003745ed73385fae76e9aaaef40d
Marianne Metais <metais.marianne@gmail.com>
2019-07-15 14:05:46 +0200

    Documentation for BaseActor

--- Content/Scripts/actors/base_actor.py
+++ Content/Scripts/actors/base_actor.py
@@ -22,10 +22,23 @@ class BaseActor():
         self.is_valid = True
 
     def actor_destroy(self):
+        """ Destroys the actor. """
         self.actor.actor_destroy()
         self.actor = None
 
     def get_parameters(self, location, rotation, overlap, warning):
+        """ Get the parameters.
+
+        Parameters
+        ----------
+        location: FVector
+            Location of the actor
+        rotation: FRotator
+            Rotation of the actor
+        overlap: bool
+        warning: bool
+
+        """
         self.location = location
         self.rotation = rotation
         self.overlap = overlap
@@ -33,6 +46,10 @@ class BaseActor():
         self.hidden = False
 
     def set_parameters(self):
+        """ Set the parameters : set location and rotation, and call the
+        methods on_actor_hit (resp. on_actor_overlap) if an actor is
+        hit (resp. overlapped).
+        """
         self.set_location(self.location)
         self.set_rotation(self.rotation)
 
@@ -43,6 +60,9 @@ class BaseActor():
             self.actor.bind_event('OnActorHit', self.on_actor_hit)
 
     def set_location(self, location):
+        """ Set the location of the actor and raise a warning if the setting
+        failed.
+        """
         self.location = location
         if not self.actor.set_actor_location(self.location, False):
             ue.log_warning('Failed to set the location of an actor')
@@ -50,6 +70,8 @@ class BaseActor():
         return True
 
     def set_rotation(self, rotation):
+        """ Set the rotation of the actor.
+        """
         # if not self.actor.set_actor_rotation(rotation):
         # the set_actor_rotation is very strict, looks for
         # equality in asked and obtained rotation. We tolerate an
@@ -70,6 +92,18 @@ class BaseActor():
         return True
 
     def on_actor_overlap(self, me, other):
+        """
+        Actors can't overlap.
+        When an the actor is overlapping another actor, self.is_valid is
+        set at False, and a message is printed.
+
+        Parameters
+        ----------
+        me: AActor
+            The current actor
+        othe: AActor
+            The actor overlapping the current actor
+        """
         if (me == other):
             return
         message = '{} overlapping {}'.format(
@@ -78,6 +112,16 @@ class BaseActor():
         self.is_valid = False
 
     def on_actor_hit(self, me, other, *args):
+        """
+        When an the actor is hitting another actor a message is printed.
+
+        Parameters
+        ----------
+        me: AActor
+            The current actor
+        othe: AActor
+            The actor hitting the current actor
+        """
         if (other.get_name()[:5] == "Floor" or me == other):
             return
         message = '{} hitting {}'.format(
@@ -85,10 +129,24 @@ class BaseActor():
         ue.log(message)
 
     def set_hidden(self, hidden):
+        """ Set the hidden parameter
+
+        Parameters
+        ----------
+        hidden: bool
+            If hidden is True, the actor is hidden
+        """
         self.hidden = hidden
         self.actor.SetActorHiddenInGame(hidden)
 
     def get_status(self):
+        """ Get the status
+
+        Returns
+        -------
+        dict
+            Status of the actor
+        """
         status = {
             'name': self.actor.get_name(),
             # 'type': self.actor.get_name().split('_')[0],
@@ -97,6 +155,9 @@ class BaseActor():
         return status
 
     def reset(self, params):
+        """ Resets the actor with the parametres of parameters.py, hidden is
+        set at False.
+        """
         self.set_location(params.location)
         self.set_rotation(params.rotation)
         self.set_hidden(False)

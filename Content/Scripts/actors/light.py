81aed4ff5b67410c2c1c60900336272001c20aee
Mathieu Bernard <mathieu.a.bernard@inria.fr>
2019-09-10 15:24:58 +0200

    PEP8

--- Content/Scripts/actors/light.py
+++ Content/Scripts/actors/light.py
@@ -7,18 +7,18 @@ from unreal_engine.classes import SkyLightComponent, DirectionalLightComponent
 from actors.base_actor import BaseActor
 from actors.parameters import LightParams
 
-"""
-The light is... a light ? which is... lighting something ?
-There is 3 kinds of lights :
+
+class Light(BaseActor):
+    """Lighting the world.
+
+    There is 3 kinds of lights.
     Directionnal : like a spot, it points toward somewhere, you chose
         location and rotation
     SkyLight : Diffuse light which doesn't need location or rotation.
         It comes from the skysphere (therefore it needs the skyshpere)
     PointLight : like a bulb. It requires a location
-"""
 
-
-class Light(BaseActor):
+    """
     def __init__(self, world, params=LightParams()):
         types = {
             'Directional': '/Game/DirectionalLight.DirectionalLight_C',
@@ -43,17 +43,17 @@ class Light(BaseActor):
         super().get_parameters(params.location, params.rotation, True, False)
 
         if self.type == 'SkyLight':
-            self.skylight_component = self.actor.get_component_by_type(SkyLightComponent)
+            self.skylight_component = self.actor.get_component_by_type(
+                SkyLightComponent)
             self.skylight_component.SetLightColor(self.color)
             self.skylight_component.SetIntensity(1.7 + params.varIntensity)
         elif self.type == 'Directional':
-            self.directionallight_component = self.actor.get_component_by_type(DirectionalLightComponent)
+            self.directionallight_component = self.actor.get_component_by_type(
+                DirectionalLightComponent)
             self.directionallight_component.SetLightColor(self.color)
-
 
     def set_parameters(self):
         super().set_parameters()
-
 
         # deactivate the physics (we don't want the light to fall)
         # self.get_mesh().set_simulate_physics(False)
